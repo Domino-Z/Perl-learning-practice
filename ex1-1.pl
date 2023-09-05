5508e789a3b017a818dc09b3102c2e4bac1e89d4
Domino-Z <zhangh0304@yeah.net>
2023-09-05 09:32:36 +0800

    init

--- ex1-1.pl
+++ ex1-1.pl
@@ -1,6 +1,17 @@
 #!/usr/bin/perl
-@lines = `perldoc -u -fatan2`;
-foreach (@lines) {
-    s/w<([^>]+)>/\U$1/g;
-    print;
-}
+# 这一行是 perl 中的注释
+=pod 注释
+perl 也支持多行注释，
+最常用的方法是使用 POD(Plain Old Documentations) 来进行多行注释。
+=cut
+
+# 输出 "Hello, World"
+print("Hello, world\n");
+print "Hello, world\n";
+
+print "Hello, world\n";    # 双引号
+print 'Hello, world\n';    # 单引号
+
+$a = 10;
+print "a = $a\n";
+print 'a = $a\n';
