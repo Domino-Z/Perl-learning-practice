b4eaf8918ac61590975b700bf88b99f4f1d9940e
Domino-Z <zhangh0304@yeah.net>
2023-09-12 17:44:58 +0800

    given-when was deprecated !

---
+++ ex15/ex15-2.pl
@@ -0,0 +1,13 @@
+# #!/usr/bin/perl
+# use strict;
+# use warnings;
+
+# for (1 .. 105){
+#     my $what = '';
+#     given ($_) {
+#         when (not $_ % 3) { $what .= ' Fizz'; continue }
+#         when (not $_ % 5) { $what .= ' Buzz'; continue }
+#         when (not $_ % 7) { $what .= 'Sausage'}
+#     }
+#     say "$_$what";
+# }
